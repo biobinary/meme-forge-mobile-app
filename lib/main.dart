59c1abf7cf9e200ea423f0aa1b87cf80e9d24145
biobinary <mmcury2019@gmail.com>
2026-04-22 14:17:02 +0700

    feat: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'features/home/home_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+ 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: MemeMakerApp(),
     ),
   );
+  
 }
 
 class MemeMakerApp extends StatelessWidget {
